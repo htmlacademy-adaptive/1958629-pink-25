ce9783b3d2c89d198e095246303049e5dfbdde96
VlasGor <gorbachevvlas@gmail.com>
2022-05-23 15:50:08 +0300

    Адаптив всех страниц и js меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,19 +1,8 @@
-const navMenu = document.querySelector('.page-header__nav--js');
-const navToggle = document.querySelector('.page-header__button-toogle--js');
-const header = document.querySelector('.page__header--js');
-const heade = document.querySelector('.page-header__position--js');
+const navToggle = document.querySelector('.js-burger-button');
+const header = document.querySelector('.js-page-header');
+
+header.classList.add('page-header--js');
 
 navToggle.addEventListener('click', () => {
-  if (navMenu.classList.contains('nav--opened')) {
-    navMenu.classList.remove('nav--opened');
-    navToggle.classList.remove('page-header__button-toogle--opened');
-    header.classList.remove('page__header--opened');
-    heade.classList.remove('page-header__position--opened');
-  } else {
-    navMenu.classList.add('nav--opened');
-    navToggle.classList.add('page-header__button-toogle--js');
-    navToggle.classList.add('page-header__button-toogle--opened');
-    header.classList.add('page-header--opened');
-    heade.classList.add('page-header__position--opened');
-  }
+  header.classList.toggle('page-header--opened');
 });
